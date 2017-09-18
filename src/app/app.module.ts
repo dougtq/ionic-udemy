d68cfaa634f3e7c463b550f8825d0b07eb6028ec
Douglas Alves <douglas.expalves@gmail.com>
2017-09-18 15:55:21 -0300

    automated generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { BrowserModule } from '@angular/platform-browser'
+import { ErrorHandler, NgModule } from '@angular/core'
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular'
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { MyApp } from './app.component'
+import { ExampleComponent } from './../components/example/example';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { HomePage } from '../pages/home/home'
+import { ListPage } from '../pages/list/list'
+import { ExamplePage } from './../pages/example/example'
+
+
+import { StatusBar } from '@ionic-native/status-bar'
+import { SplashScreen } from '@ionic-native/splash-screen'
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-  ],
+  declarations: [MyApp, HomePage, ListPage, ExamplePage, ExampleComponent],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+  entryComponents: [MyApp, HomePage, ListPage, ExamplePage],
+  providers: [StatusBar, SplashScreen, { provide: ErrorHandler, useClass: IonicErrorHandler }]
 })
 export class AppModule {}
---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ExampleComponent } from './example/example';
+@NgModule({
+	declarations: [ExampleComponent],
+	imports: [],
+	exports: [ExampleComponent]
+})
+export class ComponentsModule {}
