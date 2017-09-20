e4dc0681d66b4d1b4b09f12f3a89392c46f194da
Douglas Alves <douglas.expalves@gmail.com>
2017-09-20 10:55:23 -0300

    updates til' class 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { ExamplePage } from './../pages/example/example';
+import { ContactPage } from './../pages/contact/contact';
 
 @Component({
   templateUrl: 'app.html'
@@ -20,19 +21,22 @@ export class MyApp {
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
+    
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
       { title: 'List', component: ListPage },
-      { title: 'Example', component: ExamplePage }
+      { title: 'Example', component: ExamplePage },
+      { title: 'Contact', component: ContactPage }
     ];
-
+    
   }
-
+  
   initializeApp() {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+      this.nav.push(ContactPage)
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
